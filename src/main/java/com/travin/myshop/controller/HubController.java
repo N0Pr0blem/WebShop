2ef2c962955c727e2877edad83e9b75d8e54cb9d
Konstantin <k.bobkov1408@gmail.com>
2023-11-06 16:30:20 +0300

    Workable security. I delete mustache and set thymeleaf

--- src/main/java/com/travin/myshop/controller/HubController.java
+++ src/main/java/com/travin/myshop/controller/HubController.java
@@ -4,11 +4,10 @@ import com.travin.myshop.domain.Product;
 import com.travin.myshop.repos.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.Map;
 
 @Controller
 public class HubController {
@@ -16,19 +15,24 @@ public class HubController {
     ProductRepository productRepository;
 
     @GetMapping
-    public String hub(Map<String, Object> model) {
+    public String home(Model model) {
+        return "home";
+    }
+
+    @GetMapping("/hub")
+    public String hub(Model model) {
         Iterable<Product> allProducts = productRepository.findAll();
-        model.put("products", allProducts);
+        model.addAttribute("products", allProducts);
         return "hub";
     }
 
-    @PostMapping
-    public String add(@RequestParam String name,@RequestParam String price, Map<String, Object> model){
-        Product product = new Product(name,price);
+    @PostMapping("/hub")
+    public String add(@RequestParam String name, @RequestParam String price, Model model) {
+        Product product = new Product(name, price);
         Iterable<Product> products = productRepository.findAll();
 
         productRepository.save(product);
-        model.put("products", products);
+        model.addAttribute("products", products);
 
         return "hub";
     }
