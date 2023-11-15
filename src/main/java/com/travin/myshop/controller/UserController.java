a09bbfc60ecedf25ab699a6d265de9c7d4e41a06
Konstantin <k.bobkov1408@gmail.com>
2023-11-15 17:07:32 +0300

    Switched to the old version and edit buttons in header block

--- src/main/java/com/travin/myshop/controller/UserController.java
+++ src/main/java/com/travin/myshop/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.travin.myshop.controller;
 
+import com.travin.myshop.domain.Role;
 import com.travin.myshop.domain.User;
 import com.travin.myshop.repos.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/user")
@@ -19,5 +23,12 @@ public class UserController {
         Iterable<User> allUsers = userRepository.findAll();
         model.addAttribute("users", allUsers);
         return "userList";
+    }
+    @GetMapping("{user}")
+    public String userList(@RequestParam User user, Model model) {
+        Iterable<Role> roles = List.of(Role.values());
+        model.addAttribute("user", user);
+        model.addAttribute("roles", roles);
+        return "user-edit";
     }
 }
