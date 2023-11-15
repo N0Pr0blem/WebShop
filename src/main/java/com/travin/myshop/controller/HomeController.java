566b39816c9762fe62926f3aa24b07332b0882a1
Konstantin <k.bobkov1408@gmail.com>
2023-11-15 17:44:03 +0300

    add hiding of buttons

--- src/main/java/com/travin/myshop/controller/HomeController.java
+++ src/main/java/com/travin/myshop/controller/HomeController.java
@@ -3,6 +3,8 @@ package com.travin.myshop.controller;
 import com.travin.myshop.domain.Product;
 import com.travin.myshop.repos.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,14 +12,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HubController {
+public class HomeController {
     @Autowired
     ProductRepository productRepository;
 
     @GetMapping("/home")
     public String home(Model model) {
         Iterable<Product> allProducts = productRepository.findAll();
+        String login = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
         model.addAttribute("products", allProducts);
+        model.addAttribute("login", login);
         return "home";
     }
     @GetMapping
