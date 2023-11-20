3a5802926a7509d4654207443c5fa383036ef290
Konstantin <k.bobkov1408@gmail.com>
2023-11-20 21:44:32 +0300

    Edit header

--- src/main/java/com/travin/myshop/controller/HomeController.java
+++ src/main/java/com/travin/myshop/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.travin.myshop.controller;
 
 import com.travin.myshop.domain.Product;
 import com.travin.myshop.repos.ProductRepository;
+import com.travin.myshop.repos.UserRepository;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,15 +17,16 @@ import java.security.Principal;
 public class HomeController {
     @Autowired
     ProductRepository productRepository;
+    @Autowired
+    UserRepository userRepository;
     Logger log = LogManager.getLogger(UserController.class);
 
     @GetMapping("/home")
-    public String home(Model model) {
+    public String home(Model model,Principal principal) {
         Iterable<Product> allProducts = productRepository.findAll();
-        String login = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
         model.addAttribute("products", allProducts);
-        model.addAttribute("login", login);
-        log.info(login);
+        model.addAttribute("roles", userRepository.findByUsername(principal.getName()).getRoles());
+        log.info(userRepository.findByUsername(principal.getName()).getRoles());
         return "home";
     }
 
